84aedfbb015464ee22c693f97444c00250ef5f66
amsalem <amer.salem.ext@nokia.com>
2023-01-31 16:00:28 +0200

    remove threads fix error message

--- src/main/java/com/example/ServerPool/controller/AllocateMemoryController.java
+++ src/main/java/com/example/ServerPool/controller/AllocateMemoryController.java
@@ -2,8 +2,13 @@ package com.example.ServerPool.controller;
 import com.example.ServerPool.model.Server;
 import com.example.ServerPool.service.ResourceManagementService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RestController
@@ -17,8 +22,18 @@ public class AllocateMemoryController {
         return resourceManagementService.getServerById(id);
     }
     @PostMapping("{memory}")
-    public String allocateMemory(@PathVariable int memory) throws Exception {
-        return resourceManagementService.allocateMemory(memory);
+    public ResponseEntity<String> allocateMemory(@PathVariable int memory)
+            throws ResponseStatusException
+    {
+        try
+        {
+            String response = resourceManagementService.allocateMemory(memory);
+            return ResponseEntity.ok(response);
+        }
+        catch (ResponseStatusException e)
+        {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
     @DeleteMapping("{id}")
     public void deleteServerById(@PathVariable int id){
--- src/main/java/com/example/ServerPool/service/ResourceManagementService.java
+++ src/main/java/com/example/ServerPool/service/ResourceManagementService.java
@@ -4,67 +4,97 @@ import com.example.ServerPool.model.Server;
 import com.example.ServerPool.response.AllocateMemoryResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.aerospike.repository.AerospikeRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class ResourceManagementService {
-    private final int maxServerCapacity = 100;
+public class ResourceManagementService
+{
+
+    private final int MaxServerCapacity = 100;
     @Autowired
     private AerospikeRepository repository;
 
-    public Server getServerById(int id) {
+    public Server getServerById(int id)
+    {
         return (Server) repository.findById(id).orElse(null);
     }
 
-    public void deleteServerById(int id) {
+    public void deleteServerById(int id)
+    {
         repository.deleteById(id);
     }
 
-    public synchronized String allocateMemory(int memory) throws Exception {
-        if (memory > maxServerCapacity) {
-            throw new Exception("Sorry not enough resources, requested memory is bigger than maxServerCapacity");
-        }
+    public List<Server> findAllServer()
+    {
+        return (List<Server>) repository.findAll();
+    }
+    public synchronized String allocateMemory(int memory) throws ResponseStatusException
+    {
+
         AllocateMemoryResponse message = new AllocateMemoryResponse();
-        Thread thread = new Thread(() -> {
+
+        if (memory > MaxServerCapacity)
+        {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "Requested memory is too large.");
+        }
             Server server = findServerWithEnoughMemory(memory);
-            if (server != null) {
+            if (server != null)
+            {
                 server.setAllocatedMemory(server.getAllocatedMemory() + memory);
-                message.setMessage("user allocated " + memory + "GB of memory from server: " + server.getId());
+                message.setMessage("user allocated " + memory +
+                        "GB of memory from server: " + server.getId());
+
                 repository.save(server);
-            } else {
-                Server lNewServer = new Server((int)repository.count(), "server" + (int)repository.count(), maxServerCapacity, 0, "Creating");
-                try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException e) {
+            }
+            else
+            {
+                int lServerId = (int)repository.count();
+
+                Server lNewServer = new Server(lServerId,
+                        "server" + lServerId, MaxServerCapacity,
+                        0, "Creating");
+                try
+                {
+                    Thread.sleep(2000);
+                }
+
+                catch (InterruptedException e)
+                {
                     System.out.println(e.getMessage());
                 }
                 lNewServer.setStatus("active");
                 lNewServer.setAllocatedMemory(memory);
-                message.setMessage("user allocated " + memory + "GB of memory from server: " + lNewServer.getId());
+
+            message.setMessage("user allocated " + memory
+                        + "GB of memory from server: " + lNewServer.getId());
+
                 repository.save(lNewServer);
             }
-        });
-        thread.start();
-        thread.join();
         return message.getMessage();
     }
 
-    public List<Server>findAllServer(){
-        return (List<Server>)repository.findAll();
-    }
-    private Server findServerWithEnoughMemory(int memory) {
-        List <Server> servers1 = findAllServer().stream().sorted(Comparator.comparing(Server::getId)).filter(server ->server.getAllocatedMemory() <= maxServerCapacity-memory)
+    private Server findServerWithEnoughMemory(int memory)
+    {
+        List<Server> lServers = findAllServer().stream().sorted(Comparator.
+                        comparing(Server::getId)).filter(server ->
+                        server.getAllocatedMemory() <= MaxServerCapacity - memory)
                 .collect(Collectors.toList());
-        for (Server server : servers1) {
-            if ((server.getStatus().equals("active"))){
+        for (Server server : lServers)
+        {
+            if ((server.getStatus().equals("active")))
+            {
                 return server;
+            }
         }
-    }
         return null;
-
     }
 }
 
